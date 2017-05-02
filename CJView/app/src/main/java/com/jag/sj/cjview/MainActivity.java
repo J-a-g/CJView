6febaf8b514a05400862b38aa80be2a747a948b8
JAG <13282221172@163.com>
2017-05-02 15:51:33 +0800

    修改在Menu关闭状态下，点击带左滑时，点击事件无法分发到子View中

--- CJView/app/src/main/java/com/jag/sj/cjview/MainActivity.java
+++ CJView/app/src/main/java/com/jag/sj/cjview/MainActivity.java
@@ -2,12 +2,61 @@ package com.jag.sj.cjview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.jag.sj.horizontalview.CJHorizontalView;
+import com.jag.sj.horizontalview.CJViewInterface;
+
+public class MainActivity extends AppCompatActivity implements CJHorizontalView.CallBack2View{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final CJViewInterface cjViewInterface = (CJViewInterface) findViewById(R.id.cjhorizontalview);
+        cjViewInterface.setCallBack2View(this);
+        Button button = (Button) findViewById(R.id.btn1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v("tag","---> btn1");
+            }
+        });
+        Button button1 = (Button) findViewById(R.id.btn2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.v("tag","--->btn2");
+            }
+        });
+        Button btn_open = (Button) findViewById(R.id.btn_open);
+        btn_open.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                cjViewInterface.openMenuView();
+            }
+        });
+    }
+
+    @Override
+    public void opening() {
+        Log.v("tag","opening.......");
+    }
+
+    @Override
+    public void opened() {
+        Log.v("tag","opened.......");
+    }
+
+    @Override
+    public void closing() {
+        Log.v("tag","closing.......");
+    }
+
+    @Override
+    public void closed() {
+        Log.v("tag","closed.......");
     }
 }
